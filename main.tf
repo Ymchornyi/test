ecba01b437e06c83e62b9263a783c5ace8d8161d
Ymchornyi <yuricsson01@gmail.com>
2022-05-09 14:59:21 +0300

    test

---
+++ main.tf
@@ -0,0 +1,21 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+    }
+  }
+}
+provider "azurerm" {
+  features {}
+  subscription_id = "59853d80-0217-4a7e-b2ac-fb916fa8741f"
+  client_id       = "5c8fb416-823f-4127-ac27-e0067a0a14ee"
+  client_secret   = "bOY9-PeF9OXqbfTWt8pW8j5dbEzgVFOc0~"
+  tenant_id       = "62fb2cad-cc8f-4f17-a0da-baf4862a7191"
+
+
+}
+
+resource "azurerm_resource_group" "very-test-rg" {
+  name     = "my-rf-rg1111"
+  location = "West Europe"
+}
